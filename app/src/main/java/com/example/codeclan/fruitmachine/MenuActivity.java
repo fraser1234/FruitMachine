06ea23808e222ad7fa7a4d24e4668f6058152b3f
fraser1234 <fraseralexanderblack@gmail.com>
2018-01-08 16:00:04 +0000

    starting menu

---
+++ app/src/main/java/com/example/codeclan/fruitmachine/MenuActivity.java
@@ -0,0 +1,22 @@
+package com.example.codeclan.fruitmachine;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+
+public class MenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.activity_main, menu);
+        return true;
+    }
+}
