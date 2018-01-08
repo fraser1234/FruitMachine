80f8c7f8538a798778d8a3dc0a93a230d950716f
fraser1234 <fraseralexanderblack@gmail.com>
2018-01-08 16:35:53 +0000

    next stage to work on moving images

--- app/src/main/java/com/example/codeclan/fruitmachine/MainActivity.java
+++ app/src/main/java/com/example/codeclan/fruitmachine/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.codeclan.fruitmachine;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -34,6 +36,7 @@ public class MainActivity extends AppCompatActivity {
         images3 = (ImageView) findViewById(R.id.images3);
         images4 = (ImageView) findViewById(R.id.images4);
         images5 = (ImageView) findViewById(R.id.images5);
+
 
 
         button.setOnClickListener(new View.OnClickListener() {
@@ -145,6 +148,14 @@ public class MainActivity extends AppCompatActivity {
 
         });
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.activity_main, menu);
+        return true;
+    }
+
 
 
 }
